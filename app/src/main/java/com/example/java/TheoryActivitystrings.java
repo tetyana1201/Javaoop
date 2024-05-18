22fb12cedf14cfbe6ce777271aecaa8e69e96fb7
Tetyana Hrechyn <thretscyn@gmail.com>
2024-05-18 14:39:51 +0200

    Додавання теорії до "Рядки"

--- app/src/main/java/com/example/java/TheoryActivitystrings.java
+++ app/src/main/java/com/example/java/TheoryActivitystrings.java
@@ -1,16 +1,17 @@
 package com.example.java;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+        import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.widget.Toolbar;
 
-import android.graphics.Color;
-import android.os.Bundle;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.style.BackgroundColorSpan;
-import android.text.style.ForegroundColorSpan;
-import android.text.style.StyleSpan;
-import android.widget.TextView;
+        import android.graphics.Color;
+        import android.os.Bundle;
+        import android.text.Spannable;
+        import android.text.SpannableString;
+        import android.text.style.BackgroundColorSpan;
+        import android.text.style.ForegroundColorSpan;
+        import android.text.style.StyleSpan;
+        import android.util.Log;
+        import android.widget.TextView;
 
 public class TheoryActivitystrings extends AppCompatActivity {
 
@@ -18,87 +19,74 @@ public class TheoryActivitystrings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_theory_activitystrings);
+
         // Отримуємо посилання на Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
 
         // Встановлюємо текст на Toolbar
-        toolbar.setTitle("Основні поняття");
+        toolbar.setTitle("Рядки");
 
-        TextView textView = findViewById(R.id.textView);
-        displayTheory(textView);
+        // Отримуємо посилання на TextView
+        TextView textstringsView = findViewById(R.id.textstringsView);
+        displayTheory(textstringsView);
     }
 
-    private void displayTheory(TextView textView) {
-        String theoryText = "Змінні" + "\n" +
-                "Змінна - це контейнер, який зберігає дані. Ти можеш уявити її як коробку, в яку можна покласти предмет, потім дістати і покласти новий." + "\n" +
-                "Наприклад:" + "\n" +
-                "  int a = 5;" + "\n" +
-                "В Java є різні типи змінних:" + "\n" +
-                "  • String - зберігає текст, наприклад, \"Hello\" . Рядкові значення беруться в подвійні лапки \"' ;\n" +
-                "  • int - зберігає цілі числа без десяткових знаків, наприклад, 123 або -123 ;" + "\n" +
-                "  • boolean - зберігає два можливі значення: true (істине) або false (хибне)." + "\n" +
-                "Щоб створити змінну, потрібно вказати її тип, назву та присвоїти їй значення:" + "\n" +
-                "type name = value;\n" +
-                "Де:\n" +
-                "  • type - це один із типів даних Java, наприклад, int , String або інший;\n" +
-                "  • name - це назва змінної, наприклад, firstName або LastName ;\n" +
-                "  • value - це значення змінної.\n" +
-                "Назва змінної потрібна для того, щоб отримати значення, яке в ній записано, наприклад, для виведення в консоль - System.out. println (name);\n" +
-                "Знак рівності використовується для присвоєння змінній значення.\n" +
-                "Зверни увагу: назви змінних потрібно писати англійською мовою. Також рекомендуємо називати змінні в lowerCamelCase.\n" +
-                "Наприклад:\n" +
-                "String name = \"Bob\";\n" +
-                "boolean isMarried = true;\n" +
-                "Оголошення та ініціалізація змінної\n" +
-                "Важливо розуміти різницю між оголошенням та ініціалізацією змінної.\n" +
-                "Оголошення змінної - це процес створення змінної з таким синтаксисом: type variableName.\n" +
-                "Ініціалізація змінної - це процес присвоєння значення змінній, яка вже створена.\n" +
-                "Зазвичай, оголошення та ініціалізація змінної відбуваються водночас:\n" +
-                "int number = 5;\n" +
-                "Коментарі\n" +
-                "При написанні програми може бути корисним залишати коментарі до коду, який ти пишеш. Коментарі використовуються для того, щоб через деякий час, коли доведеться покращувати або доповнювати наявний код, було простіше розібратись, що відбувається.\n" +
-                "Для того, щоб компʼютер проігнорував певні символи та не пробував їх опрацьовувати, тобі потрібно явно вказати про це за допомогою спецсимволів коментування: // , /*\n" +
-                "та */. \n" +
-                "Є два види коментарів: \n" +
-                "Однорядкові коментарі починаються з двох знаків слеш //.\n" +
-                "Наприклад:\n" +
-                "// Це однорядковий коментар\n" +
-                "Багаторядкові коментарі починаються з /* і закінчуються */ .\n" +
-                "Наприклад:\n" +
-                "/* Це багаторядковий коментар.\n" +
-                "Java його проігнорує */\n" +
-                "Відображення інформації в консолі\n" +
-                "Під час написання програм буде корисним навчитись відображати певну інформацію в консолі (виводити на екран).\n" +
-                "В ролі даних, які корисно показати, можуть бути поточні значення змінних або результати різних операцій, наприклад, додавання чи віднімання. Щоб це зробити, потрібно використати конструкцію:\n" +
-                "System.out.println (\"Повідомлення\");\n" +
-                "Де  \"Повідомлення\" - це будь-який текст, який ти хочеш вивести. Крім тексту, це може бути й інша інформація, наприклад, значення змінної чи число.";
+    private void displayTheory(TextView textstringsView) {
+        String theorystringsText = "Рядки" + "\n" +
+                "Для роботи з рядками в Java є класи String та StringBuilder, які надають ряд методів для маніпуляції рядками." + "\n" +
+                "Методи класу String" +"\n" +
+                "toUpperCase() тa toLowerCase()" + "\n" +
+                "Методи переводять усі символи рядку у верхній або нижній регістр." +"\n" +
+                "toCharArray()" + "\n" +
+                "Метод створює новий масив та копіює кожен наступний символ рядку в елемент масиву.\n" +
+                "indexOf(char targetChar)" + "\n" +
+                "Метод повертає індекс, під яким символ уперше зʼявляється в рядку.\n" +
+                "length()" + "\n" +
+                "Метод повертає довжину рядку." + "\n" +
+                "isEmpty()" + "\n" +
+                "Метод повертає true, коли довжина рядку дорівнює 0. В іншому випадку, метод поверне false.\n" +
+                "Методи класу StringBuilder"+ "\n" +
+                "Клас StringBuilder використовується для створення рядків символів, які часто змінюються Рекомендуємо використовувати клас StringBuilder обʼєднання рядків, замість + ." + "\n" +
+                "append()" + "\n" +
+                "Метод конкатенує дане значення рядку до поточного. append () можна викликати послідовно декілька разів. Цей метод приймає типи boolean , char, int, long, String тощо." + "\n" +
+                "reverse()" + "\n" +
+                "Метод замінює поточне значення рядку на його дзеркальне відображення (зворотню послідовність символів)." + "\n" +
+                "toString()" + "\n" +
+                "Метод повертає рядок, що представляє поточне значення. Цей метод може бути корисним, коли ти працюєш із рядком StringBuilder , але тип значення, що\n" +
+                "повертається з методу - String." + "\n" +
+                "Arrays.toString()" + "\n" +
+                "Метод Arrays.toString() використовується для перетворення масиву в рядок, де кожен елемент масиву розділяється комою.";
 
-        SpannableString spannableString = new SpannableString(theoryText);
+
+                SpannableString spannableString = new SpannableString(theorystringsText);
 
         // Кольори тексту та фону, жирний та курсив
-        String[] words = {"Змінні", "Змінна", "int a = 5;", "type name = value;", "System.out. println (name);", "Зверни увагу","String name = \"Bob\";", "boolean isMarried = true;", "Важливо", "Оголошення та ініціалізація змінної", "type variableName", "Коментарі", "Однорядкові", "Багаторядкові","Відображення інформації в консолі","Повідомлення","// Це однорядковий коментар","/* Це багаторядковий коментар.", "Java його проігнорує */"};
-        int[] textColors = {Color.DKGRAY, Color.DKGRAY, Color.MAGENTA, Color.MAGENTA, Color.MAGENTA, Color.DKGRAY, Color.MAGENTA, Color.MAGENTA, Color.DKGRAY, Color.DKGRAY, Color.MAGENTA, Color.DKGRAY, Color.DKGRAY, Color.DKGRAY, Color.DKGRAY, Color.MAGENTA, Color.MAGENTA, Color.MAGENTA, Color.MAGENTA}; //колір тексту
-        int[] bgColors = {Color.TRANSPARENT, Color.TRANSPARENT, Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.TRANSPARENT, Color.YELLOW, Color.YELLOW, Color.TRANSPARENT, Color.TRANSPARENT, Color.YELLOW, Color.TRANSPARENT, Color.TRANSPARENT, Color.TRANSPARENT, Color.TRANSPARENT, Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW}; //колір фону
-        boolean[] bold = {true, false, false, false, false, true, false, false, true, true, false, true, true, true, true, false, false, false, false}; // жирний TRANSPARENT-прозорий
-        boolean[] italic = {false, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true}; // курсив
+        String[] words = {"Рядки", "Методи класу String", "toUpperCase() тa toLowerCase()", "toCharArray()", "indexOf(char targetChar)", "length()", "isEmpty()", "Методи класу StringBuilder", "append()", "reverse()", "toString()", "Arrays.toString()"};
+        int[] textColors = {Color.DKGRAY, Color.DKGRAY, Color.DKGRAY, Color.DKGRAY, Color.DKGRAY, Color.DKGRAY, Color.DKGRAY, Color.DKGRAY, Color.DKGRAY, Color.DKGRAY, Color.DKGRAY, Color.DKGRAY}; //колір тексту
+        int[] bgColors = {Color.TRANSPARENT, Color.TRANSPARENT, Color.TRANSPARENT, Color.TRANSPARENT, Color.TRANSPARENT, Color.TRANSPARENT, Color.TRANSPARENT, Color.TRANSPARENT, Color.TRANSPARENT, Color.TRANSPARENT, Color.TRANSPARENT, Color.TRANSPARENT}; //колір фону
+        boolean[] bold = {true, true, true, true, true, true, true, true, true, true, true, true}; // жирний TRANSPARENT-прозорий
+        boolean[] italic = {true, true, true, true, true, true, true, true, true, true, true, true}; // курсив
 
         for (int i = 0; i < words.length; i++) {
-            int startIndex = theoryText.indexOf(words[i]);
+            int startIndex = theorystringsText.indexOf(words[i]);
             int endIndex = startIndex + words[i].length();
 
-            if (bold[i]) {
-                spannableString.setSpan(new StyleSpan(android.graphics.Typeface.BOLD), startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-            }
-            if (italic[i]) {
-                spannableString.setSpan(new StyleSpan(android.graphics.Typeface.ITALIC), startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-            }
+            if (startIndex >= 0) { // Перевірка, чи знайдено слово в рядку
+                if (bold[i]) {
+                    spannableString.setSpan(new StyleSpan(android.graphics.Typeface.BOLD), startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                }
+                if (italic[i]) {
+                    spannableString.setSpan(new StyleSpan(android.graphics.Typeface.ITALIC), startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                }
 
-            spannableString.setSpan(new ForegroundColorSpan(textColors[i]), startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-            spannableString.setSpan(new BackgroundColorSpan(bgColors[i]), startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                spannableString.setSpan(new ForegroundColorSpan(textColors[i]), startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                spannableString.setSpan(new BackgroundColorSpan(bgColors[i]), startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            } else {
+                Log.e("TheoryActivitystrings", "Слово не знайдено: " + words[i]);
+            }
         }
 
-        textView.setText(spannableString);
-
-
+        textstringsView.setText(spannableString);
     }
 }
+
